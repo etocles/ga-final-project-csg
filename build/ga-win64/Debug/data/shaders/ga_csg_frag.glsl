a4669fdd710f06d9cb8c1864f9c424676c1a7788
Misha Pozdniakov <misha.pozd@gmail.com>
2022-04-12 15:46:48 -0400

    Updated frag shader

--- build/ga-win64/Debug/data/shaders/ga_csg_frag.glsl
+++ build/ga-win64/Debug/data/shaders/ga_csg_frag.glsl
@@ -2,7 +2,18 @@
 
 uniform vec3 u_color;
 
+const vec3 light_dir = normalize(vec3(1,-.5,1));
+
+const vec3 ambient = vec3(0.05, 0.05, 0.05);
+const vec3 diffuse = vec3(.5,.5,.5);
+
 void main(void)
 {
-	gl_FragColor = vec4(u_color, 1.0);
+	
+//	if (u_lighting_enabled)
+//	{
+	vec3 diffuse_color = diffuse * max(dot(-light_dir, o_normal),0);
+	vec3 lit_color = ambient + diffuse_color;
+	gl_FragColor = vec4(lit_color * u_color,1.0);
+//	}
 }
