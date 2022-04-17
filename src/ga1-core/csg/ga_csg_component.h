dd9841c5c26cc34232e404d00f51c5978058edf4
Misha Pozdniakov <misha.pozd@gmail.com>
2022-04-17 18:33:28 -0400

    Forgot to push this empty constructor deletion

--- src/ga1-core/csg/ga_csg_component.h
+++ src/ga1-core/csg/ga_csg_component.h
@@ -23,7 +23,6 @@
 class ga_csg_component :public ga_component
 {
 public:
-	ga_csg_component(class ga_entity* ent);
 	ga_csg_component(ga_entity* ent, ga_csg::Shape which_shape, ga_vec3f translation = { 0.0f,0.0f,0.0f }, ga_vec3f color = { 1.0f,1.0f,1.0f });
 	ga_csg_component(ga_entity* ent, ga_csg& csg1, ga_csg& csg2);
 	virtual ~ga_csg_component();
