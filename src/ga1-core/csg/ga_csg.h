46215f58b708487d391c84b814b75cba59a7aca9
Misha Pozdniakov <misha.pozd@gmail.com>
2022-04-25 15:14:12 -0400

    Adding documentation

--- src/ga1-core/csg/ga_csg.h
+++ src/ga1-core/csg/ga_csg.h
@@ -15,31 +15,57 @@
 #include "framework/ga_frame_params.h"
 #include "graphics/ga_material.h"
 
-/*
-** The main architecture for CSG.
-** Also implements assemble_drawcall() which is used
-** to draw things to the screen with OpenGL using VAOs and VBOs
-*/
+
+
+/// <summary>
+/// An abstract representation of CSG, complete with the Union, Subtract, and Intersect operations
+/// As well as modifications in space, such as translation, scale, and extrude.
+/// Uses VAOs as well as shaders to draw to the screen.
+/// Has name and color attributes as commodities. 
+/// </summary>
 class ga_csg
 {
 public:
 	static enum class Shape { CUBE, SPHERE, PYRAMID };
 	static enum class OP { ADD, SUB, INTERSECT};
 
+	/// <summary>
+	/// Creates an instance of the ga_csg class, colored white, resembling the provided shape enum
+	/// Sets name to the name of the primitive
+	/// </summary>
+	/// <param name="shp"> The primitive shape to be created </param>
 	ga_csg(Shape shp);
+
+	/// <summary>
+	/// Creates an instance of the ga_csg class with the same properties as another (color, polygons)
+	/// Sets name to the same name of the csg being copied
+	/// </summary>
+	/// <param name="other"> The ga_csg instance to be duplicated </param>
 	ga_csg(ga_csg& other);
+
+	/// <summary>
+	/// Creates an instance of the ga_csg class, colored white, from the specified polygons
+	/// Sets name to "Poly"
+	/// </summary>
+	/// <param name="polys"> A vector of polygons which create a mesh </param>
 	ga_csg(std::vector<ga_polygon>& polys);
 	~ga_csg() {
 		glDeleteVertexArrays(1, (GLuint*)&_vao);
 		glDeleteBuffers(3, _vbos);
 	};
 
-	/* Retrieve the CSG's polygons as they appear in unit-space */
+	
+	/// <summary>
+	/// Retrieve the CSG's polygons as they appear in unit-space
+	/// </summary>
+	/// <returns> Vector of polygons of the CSG centered at the origin, without transformations or scaling applied </returns>
 	std::vector<ga_polygon> get_polygons_raw() { return _polygons; };
 
-	/* Retrieve a certain CSG object's polygons with transformations
-	*  !! VERY EXPENSIVE OPERATION !!
-	*/
+	/// <summary>
+	/// Retrieve a certain CSG object's polygons with transformations
+	/// VERY computationally and memory intensive operation.
+	/// </summary>
+	/// <returns> Vector of polygons of the CSG with transformations and scalings applied </returns>
 	std::vector<ga_polygon> get_polygons() {
 		std::vector<ga_polygon> res;
 		for (int i = 0; i < _polygons.size(); i++) {
@@ -56,20 +82,85 @@ public:
 		return res;
 	}
 
+	/// <summary>
+	/// Performs the Add operation on two CSG objects also represented 
+	/// in the the form this + other.
+	/// </summary>
+	/// <param name="other"> The other csg to perform union with </param>
+	/// <returns> A new csg which has polygons of both csgs </returns>
 	ga_csg add(ga_csg& other);
+	/// <summary>
+	/// Performs the Subtract operation on two CSG objects also represented 
+	/// in the the form this - other.
+	/// </summary>
+	/// <param name="other"> The other csg to perform union with </param>
+	/// <returns> A new csg which has polygons of both csgs </returns>
 	ga_csg subtract(ga_csg& other);
+	/// <summary>
+	/// Performs the Intersect operation on two CSG objects also represented 
+	///	in the the form this XOR other.
+	/// </summary>
+	/// <param name="other"> The other csg to perform union with </param>
+	/// <returns> A new csg which has polygons of both csgs </returns>
 	ga_csg intersect(ga_csg& other);
 
+	/// <summary>
+	/// Creates a primitive unit length cube centered at the origin
+	/// </summary>
+	/// <returns> A csg instance that of a cube centered at origin </returns>
 	ga_csg static Cube();
+	/// <summary>
+	/// Creates a primitive unit length sphere centered at the origin
+	/// </summary>
+	/// <returns> A csg instance that of a sphere centered at origin </returns>
 	ga_csg static Sphere();
+	/// <summary>
+	/// Creates a primitive unit length pyramid centered at the origin
+	/// </summary>
+	/// <returns> A csg instance that of a pyramid centered at origin </returns>
 	ga_csg static Pyramid();
-
+	/// <summary>
+	/// Sets a new color for the csg
+	/// </summary>
+	/// <param name="col"> The new color to change to, following the format {r,g,b} </param>
 	void set_color(ga_vec3f col) { _color = col; _material->set_color(col); };
+	/// <summary>
+	/// Sets a new position for the csg
+	/// </summary>
+	/// <param name="col"> A position in 3D space, following the format {x,y,z} </param>
 	void set_pos(ga_vec3f t);
+	/// <summary>
+	/// Sets a new scale for the csg
+	/// </summary>
+	/// <param name="col"> The new scale of the object, following the format {x,y,z} </param>
 	void set_scale(ga_vec3f t);
+	/// <summary>
+	/// Extrudes an object in the specified direction (dir) an amount (amt)
+	/// </summary>
+	/// <param name="dir">
+	/// The direction, or, "face" to extrude.
+	/// </param>
+	/// <param name="amt"> 
+	/// A value greater than 0, which resembles how much to extrude by
+	/// 0 < amt < 1 will extrude the object inwards by amt*100 percent
+	/// 1 < amt will extrude the object's face outwards by (1-amt)*100 percent
+	/// </param>
+	/// <remarks>
+	/// For example, calling extrude with the parameters (0,1,0) and (1.2) will make the
+	/// top of the object extend, leaving it unchanged in 3D space.
+	/// Calling extrude with the parameters (0,1,0) and (0.8) will instead make the top of the object 
+	/// extrude inwards.
+	/// </remarks>
 	void extrude(ga_vec3f dir, float amt);
-
+	/// <summary>
+	/// Obtain the transform matrix this object uses to appear in 3D space
+	/// </summary>
+	/// <returns> A 4D matrix of floats representing translation and scale </returns>
 	ga_mat4f get_transform() { return _transform; };
+	/// <summary>
+	/// Obtain the material of the object for modification
+	/// </summary>
+	/// <returns> A pointer to the material attached to this csg </returns>
 	ga_csg_material* get_material() { return _material; };
 
 	std::string name;
--- src/ga1-core/csg/ga_csg_component.h
+++ src/ga1-core/csg/ga_csg_component.h
@@ -17,23 +17,68 @@
 #include <cstdint>
 #include <string>
 
-/*
-** A component that attaches to the viper engine in order to provide
-** CSG functionality
-*/
+/// <summary>
+/// A component that attaches to an entity in the viper engine.
+/// Stores multiple references to csg objects which can be modified individually
+/// As well as combined with Add, Subtract, and Intersect to create new ones.
+/// </summary>
 class ga_csg_component :public ga_component
 {
 public:
+	/// <summary>
+	/// Initializes a component with a primitive shape
+	/// </summary>
+	/// <param name="ent"> A pointer to the entity to which this component gets added to </param>
+	/// <param name="which_shape"> An enum value dictating which primitive to spawn with the creation of this component</param>
+	/// <param name="translation"> An optional parameter, specifying where to position the spawned CSG in 3D space</param>
+	/// <param name="color"> An optional parameter, specifying which color to give the spawned primitive</param>
 	ga_csg_component(ga_entity* ent, ga_csg::Shape which_shape, ga_vec3f translation = { 0.0f,0.0f,0.0f }, ga_vec3f color = { 1.0f,1.0f,1.0f });
+	/// <summary>
+	/// Initializes a component with the resultant of performing csg1 op csg2
+	/// </summary>
+	/// <param name="ent"> A pointer to the entity to which this component gets added to </param>
+	/// <param name="csg1"> A reference to the csg object which is performing the operation </param>
+	/// <param name="csg2"> A reference to the csg object which is the second argument in the operation </param>
+	/// <param name="op"> An enum specifying which operation to be performed </param>
 	ga_csg_component(ga_entity* ent, ga_csg& csg1, ga_csg& csg2, ga_csg::OP op = ga_csg::OP::ADD);
 	virtual ~ga_csg_component();
+	
+	/// <summary>
+	/// Assembles drawcalls for all the csgs in its possession, pushing them onto the draw stack
+	/// </summary>
+	/// <param name="params"></param>
 	virtual void update(struct ga_frame_params* params) override;
+	/// <summary>
+	/// Removes any csgs queued for removal
+	/// </summary>
+	/// <param name="params"></param>
 	virtual void late_update(struct ga_frame_params* params) override;
 
+	/// <summary>
+	/// Accessor which retrieves a csg component's child csg at the specified index
+	/// </summary>
+	/// <param name="i"> Index of the child csg to return </param>
+	/// <returns> A pointer to the csg object located at that index. </returns>
 	ga_csg* get_csg(int i = 0) { return _csgs[i]; };
+	/// <summary>
+	/// Adds a csg to the list of owned csg children
+	/// </summary>
+	/// <param name="csg"> Pointer to csg object to be added </param>
 	void add(ga_csg* csg) { _csgs.push_back(csg); }
+	/// <summary>
+	/// Removes a csg from the list of owned csgs
+	/// </summary>
+	/// <param name="i"> Index of the csg to be removed </param>
 	void remove(int i);
+	/// <summary>
+	/// Returns the amount of children csgs contained
+	/// </summary>
+	/// <returns> the amount of children csgs </returns>
 	int size() { return _csgs.size(); }
+	/// <summary>
+	/// A nicety to easily assign a unique ID to each attached CSG
+	/// </summary>
+	/// <returns> the id to assign to the newly added CSG </returns>
 	int get_id() { return nonce++; }
 
 private:
